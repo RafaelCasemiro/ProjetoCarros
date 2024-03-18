72ab05eca846057ab7d378beac53a7c079ac88e5
Rafael Casemiro <rafindamassa@gmail.com>
2024-03-17 21:44:03 -0300

    sexto commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
       'green': '#006971',
       'dark-green': '#004140',
       'offwhite': 'rgba (255, 255, 255, 0.75)',
-      'white': 'fff',
+      'white': '#fff',
       'offwhitebg': '#f2f2f2'
     },
     fontFamily:{
