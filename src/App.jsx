350944512c097e9b7bca6ef8ec0a76e37a52f859
Rafael Casemiro <rafindamassa@gmail.com>
2024-03-17 23:07:28 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Eletrico from "./components/Eletrico"
 export default function App(){
   return (
       <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-        <section className="lg:flex max-w-[920px]">
+        <div className="lg:flex max-w-[920px]">
           <Sedans />
           <Suvs />
           <Eletrico />
           
-        </section>
+        </div>
       </main>
   )
 }
